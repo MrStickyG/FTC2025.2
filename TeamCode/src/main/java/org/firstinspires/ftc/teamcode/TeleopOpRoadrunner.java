9ec5864e505aa9911378a332fc26c614c7347174
vafc21 <166269599+vafc21@users.noreply.github.com>
2025-02-06 17:03:51 -0500

    Finished TelopOpRoadrunner
    Not tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpRoadrunner.java
@@ -4,8 +4,9 @@ package org.firstinspires.ftc.teamcode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.subsystem.Dispenser;
 import org.firstinspires.ftc.teamcode.subsystem.Drive;
@@ -14,7 +15,8 @@ import org.firstinspires.ftc.teamcode.subsystem.Pivot;
 
 @TeleOp(name="TeleopOp_Roadrunner")
 
-public class TeleopOpRoadrunner {
+public class TeleopOpRoadrunner extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
     Drive drive = new Drive(hardwareMap);
     Extension extension = new Extension(hardwareMap);
     Pivot pivot = new Pivot(hardwareMap);
@@ -27,6 +29,22 @@ public class TeleopOpRoadrunner {
 
         waitForStart();
         runtime.reset();
+
+        while (opModeIsActive()){
+            drive.drive(-gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x);
+            extension.extension(gamepad1.right_bumper, gamepad1.left_bumper);
+            pivot.pivot(gamepad1.left_trigger, gamepad1.right_trigger, gamepad1.x);
+            dispenser.dispenser(gamepad1.a, gamepad1.b);
+
+            // Show the elapsed game time and wheel power.
+            //telemetry.addData("Left Trigger", -intake);
+            //telemetry.addData("Right Trigger", dispense);
+            //telemetry.addData("Arm Langth", Extension.getCurrentPosition());
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            //telemetry.addData("Pivot Angle", Pivot.getCurrentPosition());
+            //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
 
     }
 
