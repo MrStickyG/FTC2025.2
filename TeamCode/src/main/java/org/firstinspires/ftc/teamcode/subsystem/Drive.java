7ed3f4612f3523ebba1f34b1de86e6531beca097
Vlad <vladimir6600@gmail.com>
2025-02-05 20:30:16 -0500

    Adding Drive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+public class Drive {
+    private DcMotor FRMotor = null;
+    private DcMotor FLMotor = null;
+    private DcMotor BRMotor;
+    private DcMotor BLMotor;
+    double DConstant=1;
+
+    public Drive(HardwareMap hardwareMap){
+        FRMotor  = hardwareMap.get(DcMotor.class, "FrontRightMotor");
+        FLMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
+        BLMotor = hardwareMap.get(DcMotor.class, "BackLeftMotor");
+        BRMotor = hardwareMap.get(DcMotor.class, "BackRightMotor");
+
+        FRMotor.setDirection(DcMotor.Direction.REVERSE);
+        BRMotor.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+    public void drive(double turn,double drive, double rotate){
+        double BLpower = DConstant* Range.clip(-drive - turn +rotate, -1.0, 1.0) ;
+        double FLpower = DConstant*Range.clip(drive - turn - rotate, -1.0, 1.0) ;
+        double FRpower = DConstant*Range.clip(drive + turn + rotate, -1.0, 1.0) ;
+        double BRpower = DConstant*Range.clip(-drive + turn - rotate, -1.0, 1.0) ;
+
+        // Send calculated power to wheels
+        FLMotor.setPower(FLpower);
+        BLMotor.setPower(BLpower);
+        BRMotor.setPower(BRpower);
+        FRMotor.setPower(FRpower);
+
+    }
+}
